e8c7de416f52430206332a7afae2e6d79a1c4fad
Rade <26564356@qq.com>
2021-03-24 20:36:47 +0800

    update ignore

--- trustchina-module-system/src/main/java/org/jeecg/modules/trustchain/controller/TestController.java
+++ trustchina-module-system/src/main/java/org/jeecg/modules/trustchain/controller/TestController.java
@@ -1,5 +1,7 @@
 package org.jeecg.modules.trustchain.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.jeecg.common.api.vo.Result;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2021/3/24 20:01:01
  * @Description
  */
+@Api(tags="测试模块")
 @RestController
 @RequestMapping("/test")
 public class TestController {
+
+    @ApiOperation("测试接口")
     @GetMapping("/hello")
     public Result<String> hello() {
         return Result.OK("hello");
